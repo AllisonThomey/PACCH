816f7321623b198e6e75a44c630f8eb832437645
allison <athomey@gmu.edu>
2023-06-08 17:23:50 -0400

    fixing variables in user functions

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -53,16 +53,14 @@ def process_national_boundary(country):
 
     """
     #load in GID_0 gadm layer
-    path = os.path.join('data', 'countries.csv')
-    countries = pandas.read_csv(path, encoding='latin-1')
     filename = "gadm36_0.shp"
     folder = os.path.join('data', 'raw', 'gadm36_levels_shp')
     path_in= os.path.join(folder, filename)
     boundaries = geopandas.read_file(path_in, crs="epsg:4326")
 
-    iso3 = countries['iso3']
-    country_boundaries = boundaries[boundaries['GID_0'] == countries['iso3']]
-    gid_region = countries['gid_region']
+    iso3 = country['iso3']
+    country_boundaries = boundaries[boundaries['GID_0'] == country['iso3']]
+    gid_region = country['gid_region']
     gid_level = 'GID_{}'.format(gid_region)
 
     #extract GID_0 for the country of interest, e.g., using the iso3 code
@@ -85,54 +83,47 @@ def process_national_boundary(country):
     
     return
 
-# def process_regional_boundary(country):
-#     """
-#     this explains what the function does...
+def process_regional_boundary(country):
+    """
+    this explains what the function does...
 
 
-#     """
-#     path = os.path.join('data', 'countries.csv')
-#     countries = pandas.read_csv(path, encoding='latin-1')
+    """
 
-#     for idx, country in countries.iterrows():
+    iso3 = country["iso3"]
+    gid_region = country['gid_region']
+    gid_level = 'GID_{}'.format(gid_region)
 
-#         iso3 = country["iso3"]
-#         gid_region = country['gid_region']
-#         gid_level = 'GID_{}'.format(gid_region)
+    #prefered gid level
+    filename = 'gadm36_{}.shp'.format(country['gid_region'])
+    path_in = os.path.join('data', 'raw', 'gadm36_levels_shp', filename)
+    boundaries = geopandas.read_file(path_in)
+    country_boundaries = boundaries[boundaries['GID_0'] == country['iso3']]
 
-#         #prefered gid level
-#         filename = 'gadm36_{}.shp'.format(country['gid_region'])
-#         path_in = os.path.join('data', 'raw', 'gadm36_levels_shp', filename)
-#         boundaries = geopandas.read_file(path_in)
-#         country_boundaries = boundaries[boundaries['GID_0'] == country['iso3']]
+    #set the filename depending our preferred regional level
+    filename = "gadm36_{}.shp".format(gid_region)
+    regions_folder_path = os.path.join('data', 'processed', iso3, 'processed_regions')
+    if not os.path.exists(regions_folder_path):
+        os.makedirs(regions_folder_path)
+
+    #this is how we simplify the geometries
+    country_boundaries["geometry"] = country_boundaries.geometry.simplify(
+        tolerance=0.01, preserve_topology=True)
         
-#         #extract GID_0 for the country of interest, e.g., using the iso3 code
-#         if not country['iso3']=='BGD':
-#             continue
-#         #set the filename depending our preferred regional level
-#         filename = "gadm36_{}.shp".format(gid_region)
-#         regions_folder_path = os.path.join('data', 'processed', iso3, 'processed_regions')
-#         if not os.path.exists(regions_folder_path):
-#             os.makedirs(regions_folder_path)
-
-#         #this is how we simplify the geometries
-#         country_boundaries["geometry"] = country_boundaries.geometry.simplify(
-#             tolerance=0.01, preserve_topology=True)
-        
-#         #remove small shapes
-#         country_boundaries['geometry'] = country_boundaries.apply(
-#             remove_small_shapes, axis=1)
+    #remove small shapes
+    country_boundaries['geometry'] = country_boundaries.apply(
+        remove_small_shapes, axis=1)
                                             
-#         #set the filename depending our preferred regional level
-#         filename = "gadm36_{}.shp".format(gid_region)
+    #set the filename depending our preferred regional level
+    filename = "gadm36_{}.shp".format(gid_region)
 
-#         #saving gid region boundaries
-#         path_out = os.path.join('data', 'processed', iso3,'gid_region', filename)
-#         if not os.path.exists(path_out):
-#             os.makedirs(path_out)
-#         country_boundaries.to_file(path_out, crs='epsg:4326')
+    #saving gid region boundaries
+    path_out = os.path.join('data', 'processed', iso3,'gid_region', filename)
+    if not os.path.exists(path_out):
+        os.makedirs(path_out)
+    country_boundaries.to_file(path_out, crs='epsg:4326')
 
-#     return
+    return
 
 
 if __name__ == "__main__":
@@ -145,7 +136,8 @@ if __name__ == "__main__":
         if not country['iso3']=='BGD':
             continue
 
-        process_national_boundary(country)
+        # process_national_boundary(country)
+        process_regional_boundary(country)
         print(country)
         # #GID0
         # filename = "gadm36_0.shp"
