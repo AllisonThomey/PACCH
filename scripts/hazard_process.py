94e8f3c52fc393e7b578187ac905e41b745e73fb
allison <athomey@gmu.edu>
2023-06-12 15:00:49 -0400

    made region and national hazard function

---
+++ scripts/hazard_process.py
@@ -0,0 +1,162 @@
+#making user defined functions for processing hazard 
+#starting with code base from 470
+import os
+import json
+import rasterio
+from rasterio.mask import mask
+import pandas
+import geopandas as gpd
+
+def process_national_hazard(country):
+    """
+    This function creates a national hazard .tiff using 
+    national boundary files created in 
+    process_national_boundary function
+
+    """
+    #settign variables
+    iso3 = country['iso3']
+    gid_region = country['gid_region']
+    gid_level = 'GID_{}'.format(gid_region)
+
+    #loading in coastal flood hazard .tiff
+    filename = 'inuncoast_rcp8p5_wtsub_2080_rp1000_0.tif'
+    path_hazard = os.path.join('data','raw','flood_hazard', filename)
+    hazard = rasterio.open(path_hazard, 'r+')
+    hazard.nodata = 255                       #set the no data value
+    hazard.crs.from_epsg(4326)  
+
+    #set the filename depending our preferred regional level
+    filename = 'national_outline.shp'
+    folder = os.path.join('data', 'processed', iso3, 'national', filename)
+        
+    #then load in our country as a geodataframe
+    path_in = os.path.join(folder, filename)
+    country_shp = gpd.read_file(path_in, crs='epsg:4326')
+    
+    geo = gpd.GeoDataFrame(gpd.GeoSeries(country_shp.geometry))
+    #this line sets geometry for resulting geodataframe
+    geo = geo.rename(columns={0:'geometry'}).set_geometry('geometry')
+    #convert to json
+    coords = [json.loads(geo.to_json())['features'][0]['geometry']] 
+        
+    #carry out the clip using our mask
+    out_img, out_transform = mask(hazard, coords, crop=True)
+    out_img, out_transform
+
+    #update our metadata
+    out_meta = hazard.meta.copy()
+    out_meta.update({"driver": "GTiff",
+                    "height": out_img.shape[1],
+                    "width": out_img.shape[2],
+                    "transform": out_transform,
+                    "crs": 'epsg:4326'})
+    
+    #now we write out at the national level
+    filename_out = 'inuncoast_rcp8p5_wtsub_2080_rp1000_0.tif' 
+    folder_out = os.path.join('data', 'processed', iso3 , 'hazards', 'inuncoast', 'national')
+    if not os.path.exists(folder_out):
+        os.makedirs(folder_out)
+    path_out = os.path.join(folder_out, filename_out)
+
+    with rasterio.open(path_out, "w", **out_meta) as dest:
+        dest.write(out_img)
+
+
+    return
+
+
+def process_regional_hazard (country, region):
+    """
+   This function creates a regional composite hazrd 
+    .tif using regional boundary files created in 
+    process_regional_boundary function and national
+    population files created in process_national_population
+    function.
+
+
+    """
+    #assigning variables
+    iso3 = country['iso3']
+    gid_region = country['gid_region']
+    gid_level = 'GID_{}'.format(gid_region)
+    gid_id = region[gid_level]
+
+
+    #loading in hazard .tif
+    filename = 'inuncoast_rcp8p5_wtsub_2080_rp1000_0.tif'
+    path_hazard = os.path.join('data','raw','flood_hazard', filename)
+    hazard = rasterio.open(path_hazard, 'r+')
+    hazard.nodata = 255                       #set the no data value
+    hazard.crs.from_epsg(4326)  
+
+    geo = gpd.GeoDataFrame(gpd.GeoSeries(region.geometry))
+    #this line sets geometry for resulting geodataframe
+    geo = geo.rename(columns={0:'geometry'}).set_geometry('geometry')
+    #convert to json
+    coords = [json.loads(geo.to_json())['features'][0]['geometry']] 
+        
+    #carry out the clip using our mask
+    out_img, out_transform = mask(hazard, coords, crop=True)
+    out_img, out_transform
+
+    #update our metadata
+    out_meta = hazard.meta.copy()
+    out_meta.update({"driver": "GTiff",
+                    "height": out_img.shape[1],
+                    "width": out_img.shape[2],
+                    "transform": out_transform,
+                    "crs": 'epsg:4326'})
+   
+    #now we write out at the national level
+    filename_out = '{}.tif'.format(gid_id)
+    folder_out = os.path.join('data', 'processed', iso3 , 'hazards', 'inuncoast', gid_id)
+    if not os.path.exists(folder_out):
+        os.makedirs(folder_out)
+    path_out = os.path.join(folder_out, filename_out)
+
+    with rasterio.open(path_out, "w", **out_meta) as dest:
+        dest.write(out_img) 
+
+
+    return
+
+
+if __name__ == "__main__":
+
+    path = os.path.join('data', 'countries.csv')
+    countries = pandas.read_csv(path, encoding='latin-1')
+
+    for idx, country in countries.iterrows():
+
+        # if country['coastal_exclude'] == 1: # let's work on a single country at a time
+        #     continue   
+        if not country['iso3'] =='BGD':
+            continue
+        
+        # #define our country-specific parameters, including gid information
+        iso3 = country['iso3']
+        gid_region = country['gid_region']
+        gid_level = 'GID_{}'.format(gid_region)
+        
+        #set the filename depending our preferred regional level
+        filename = "gadm36_{}.shp".format(gid_region)
+        folder = os.path.join('data','processed', iso3, 'gid_region')
+        
+        #then load in our regions as a geodataframe
+        path_regions = os.path.join(folder, filename)
+        regions = gpd.read_file(path_regions, crs='epsg:4326')#[:2]
+        
+        # print("Working on process_national_hazard for {}".format(iso3))
+        # process_national_hazard(country)
+        
+        print("Working on process_regional_hazard")
+        
+        for idx, region in regions.iterrows():
+        # #     # # if region.geometry == None:
+        # #      continue 
+            #  if not region[gid_level] == 'BGD.1.5_1':
+            #      continue
+            
+       
+             process_regional_hazard(country, region)
