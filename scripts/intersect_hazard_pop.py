97e5f25ec33f0c11913db9f2a3c0f0f870e6b0da
allison <athomey@gmu.edu>
2023-07-05 10:28:15 -0400

    Removed region logic, added if-not-continue logic

--- scripts/intersect_hazard_pop.py
+++ scripts/intersect_hazard_pop.py
@@ -25,35 +25,45 @@ def intersect_hazard_pop(country, region):
     gid_id = region[gid_level]
 
 
-    #load in population by region .shp file
-    filename_pop = '{}'.format(gid_id) #each regional file is named using the gid id
-    path_pop = os.path.join(BASE_PATH, 'processed', iso3 , 'population', filename_pop)
-    gdf_pop =  gpd.read_file(path_pop, crs="EPSG:4326")
-    gdf_pop = gdf_pop.to_crs('epsg:3857')
+    filename = "gadm36_{}.shp".format(gid_region)
+    path_region = os.path.join('data', 'processed', iso3,'gid_region', filename)
+    gdf_region = gpd.read_file(path_region, crs="EPSG:4326")
+    gdf_region = gdf_region[gdf_region[gid_level] == gid_id]
 
-    #load in hazard .shp file
-    filename_hazard = '{}.shp'.format(gid_id)
-    path_hazard = os.path.join(BASE_PATH, 'processed', iso3 , 'hazards', 
-                               'inuncoast', gid_id, filename_hazard)
-    gdf_hazard = gpd.read_file(path_hazard, crs="EPSG:4326")
-    gdf_hazard = gdf_hazard.to_crs('epsg:3857')
+    for regions in gdf_region.iterrows():
+        #load in population by region .shp file
+        filename_pop = '{}'.format(gid_id) #each regional file is named using the gid id
+        path_pop = os.path.join(BASE_PATH, 'processed', iso3 , 'population', filename_pop)
+        gdf_pop =  gpd.read_file(path_pop, crs="EPSG:4326")
+        gdf_pop = gdf_pop.to_crs('epsg:3857')
 
-    gdf_affected = gpd.overlay(gdf_pop, gdf_hazard, how='intersection')
+        #load in hazard .shp file
+        filename_hazard = '{}.shp'.format(gid_id)
+
+        path_hazard = os.path.join(BASE_PATH, 'processed', iso3 , 'hazards', 
+                                'inuncoast', gid_id, filename_hazard)
+        if not os.path.exists(path_hazard):
+            continue
+        gdf_hazard = gpd.read_file(path_hazard, crs="EPSG:4326")
+        gdf_hazard = gdf_hazard.to_crs('epsg:3857')
+
+        gdf_affected = gpd.overlay(gdf_pop, gdf_hazard, how='intersection')
 
 
-    # area to 1 km
-    gdf_affected['area_km2'] = gdf_affected['geometry'].area/ 1e6
-    gdf_affected['pop_est'] = gdf_affected['value_1']* gdf_affected['area_km2']
-  
+        # area to 1 km
+        gdf_affected['area_km2'] = gdf_affected['geometry'].area/ 1e6
+        gdf_affected['pop_est'] = gdf_affected['value_1']* gdf_affected['area_km2']
+    
+        gdf_affected = gdf_affected.to_crs('epsg:4326')
 
-    #now we write out path at the regional level
-    filename_out = '{}'.format(gid_id) #each regional file is named using the gid id
-    folder_out = os.path.join(BASE_PATH, 'processed', iso3 , 'intersect', 'hazard_pop')
+        #now we write out path at the regional level
+        filename_out = '{}'.format(gid_id) #each regional file is named using the gid id
+        folder_out = os.path.join(BASE_PATH, 'processed', iso3 , 'intersect', 'hazard_pop')
 
-    path_out = os.path.join(folder_out, filename_out)
-    if not os.path.exists(path_out):
-        os.makedirs(path_out)
-    gdf_affected.to_file(path_out, crs='epsg:4326')
+        path_out = os.path.join(folder_out, filename_out)
+        if not os.path.exists(path_out):
+            os.makedirs(path_out)
+        gdf_affected.to_file(path_out, crs='epsg:4326')
 
 
 
@@ -89,8 +99,6 @@ if __name__ == "__main__":
         
         for idx, region in regions.iterrows():
 
-            if not region[gid_level] == 'BGD.1.5_1':
-                continue
             
        
             intersect_hazard_pop(country, region)
