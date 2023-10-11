aae74786b2fe9f420d4a1f9b59f6f8d8f69cf407
allison <athomey@gmu.edu>
2023-10-11 17:25:04 -0400

    making results folder

--- scripts/collection.py
+++ scripts/collection.py
@@ -26,53 +26,61 @@ def process_vul_pop(region):
     gdf_region = gdf_region.to_crs('epsg:3857')
     region_dict = gdf_region.to_dict('records')
     
-    output = []
-    for region in region_dict:
 
-        filename = 'coastal_lookup.csv'
-        folder = os.path.join('data', 'processed', iso3, 'coastal')
-        path_coast= os.path.join(folder, filename)
-        if not os.path.exists(path_coast):
-            continue
-        coastal = pandas.read_csv(path_coast)
-        coast_list = coastal['gid_id'].values. tolist()
+    haz_scene = ["inuncoast_historical_wtsub_2080_rp0100_0.{}", "inuncoast_historical_wtsub_2080_rp1000_0.{}", 
+                 "inuncoast_rcp4p5_wtsub_2080_rp0100_0.{}", "inuncoast_rcp4p5_wtsub_2080_rp1000_0.{}", 
+                 "inuncoast_rcp8p5_wtsub_2080_rp0100_0.{}", "inuncoast_rcp8p5_wtsub_2080_rp1000_0.{}"]
+ 
+    for scene in haz_scene:
+        output = []
+        for region in region_dict:
 
-        if not region[gid_level] in coast_list:
-            continue
-        gid_id = region[gid_level]
+            filename = 'coastal_lookup.csv'
+            folder = os.path.join('data', 'processed', iso3, 'coastal')
+            path_coast= os.path.join(folder, filename)
+            if not os.path.exists(path_coast):
+                continue
+            coastal = pandas.read_csv(path_coast)
+            coast_list = coastal['gid_id'].values. tolist()
 
-        filename_pop = '{}'.format(gid_id) #each regional file is named using the gid id
-        folder_pop = os.path.join('data', 'processed', iso3 , 'intersect', 'hazard_pop', filename_pop)
-        if not os.path.exists(folder_pop):
-            continue
-        gdf_pop = geopandas.read_file(folder_pop)
-        area = gdf_pop['area_km2'].sum()
-        vul_pop = gdf_pop['pop_est'].sum()
+            if not region[gid_level] in coast_list:
+                continue
+            gid_id = region[gid_level]
 
-        #adding total original population from worldpop for each region
-        filename_pop = '{}'.format(gid_id) #each regional file is named using the gid id
-        folder_pop = os.path.join('data', 'processed', iso3 , 'population', filename_pop)
-        if not os.path.exists(folder_pop):
-            continue
-        og_pop = geopandas.read_file(folder_pop)
-        total_pop = og_pop['value'].sum()
+            filename_pop = '{}'.format(gid_id) #each regional file is named using the gid id
+            folder_pop = os.path.join('data', 'processed', iso3 , 'intersect', 'hazard_pop', scene)
 
-        output.append({
-            'iso3': iso3,
-            'gid_id': gid_id,
-            'pop_est': vul_pop,
-            'income_group': income,
-            'continent': continent,
-            'area_km2': area,
-            'total_pop':total_pop
-        })
-    output=pandas.DataFrame(output)
-    filename_out = 'varifying_pop.csv'
-    folder_out = os.path.join('data', 'processed', iso3 , 'csv')
-    if not os.path.exists(folder_out):
-        os.makedirs(folder_out)
-    path_out = os.path.join(folder_out, filename_out)
-    output.to_csv(path_out, index = False)
+            path_pop = os.path.join(folder_pop, filename_pop)
+            if not os.path.exists(path_pop):
+                continue
+            gdf_pop = geopandas.read_file(path_pop)
+            area = gdf_pop['area_km2'].sum()
+            vul_pop = gdf_pop['pop_est'].sum()
+
+            #adding total original population from worldpop for each region
+            filename_pop = '{}'.format(gid_id) #each regional file is named using the gid id
+            folder_pop = os.path.join('data', 'processed', iso3 , 'population', scene, filename_pop)
+            if not os.path.exists(folder_pop):
+                continue
+            og_pop = geopandas.read_file(folder_pop)
+            total_pop = og_pop['value'].sum()
+
+            output.append({
+                'iso3': iso3,
+                'gid_id': gid_id,
+                'pop_est': vul_pop,
+                'income_group': income,
+                'continent': continent,
+                'area_km2': area,
+                'total_pop':total_pop
+            })
+        output=pandas.DataFrame(output)
+        filename_out = 'varifying_pop.csv'
+        folder_out = os.path.join('data', 'processed', iso3 , 'csv', scene)
+        if not os.path.exists(folder_out):
+            os.makedirs(folder_out)
+        path_out = os.path.join(folder_out, filename_out)
+        output.to_csv(path_out, index = False)
 
     return
 
@@ -83,13 +91,18 @@ if __name__ == "__main__":
     countries = pandas.read_csv(path, encoding='latin-1')
     countries = countries.to_dict('records')
 
-    output = []
-    for country in countries:
+    haz_scene = ["inuncoast_historical_wtsub_2080_rp0100_0.{}", "inuncoast_historical_wtsub_2080_rp1000_0.{}", 
+                 "inuncoast_rcp4p5_wtsub_2080_rp0100_0.{}", "inuncoast_rcp4p5_wtsub_2080_rp1000_0.{}", 
+                 "inuncoast_rcp8p5_wtsub_2080_rp0100_0.{}", "inuncoast_rcp8p5_wtsub_2080_rp1000_0.{}"]
 
+    for country in countries:
         if country['Exclude'] == 1:
             continue
 
         if country['income_group'] == 'HIC':
+            continue
+
+        if not country['iso3'] == 'BGD':
             continue
 
         iso3 = country['iso3']
@@ -102,32 +115,34 @@ if __name__ == "__main__":
         if not os.path.exists(path_coast):
             continue
 
-        # filename = '{}_relative_wealth_index.csv'.format(iso3)
-        # path_rwi = os.path.join('data','raw','rwi', filename)
-        # if not os.path.exists(path_rwi):
-        #     continue 
-
-        folder_pop = os.path.join('data', 'processed', iso3 , 'intersect', 'hazard_pop')
-        if not os.path.exists(folder_pop):
-            continue
-
         print('Working on {}'.format(iso3))
-        process_vul_pop(country)
+        # process_vul_pop(country)
 
-        filename_in = 'varifying_pop.csv'
-        folder_in = os.path.join('data', 'processed', iso3 , 'csv')
-        path_in = os.path.join(folder_in, filename_in)
-        if not os.path.exists(path_in):
-            continue
-        
-        pop = pandas.read_csv(path_in)
-        pop = pop.to_dict('records')
-        output = output + pop
+    for scene in haz_scene:
+        output = []
+        for country in countries:
+            if country['Exclude'] == 1:
+                continue
 
-    output = pandas.DataFrame(output)
-    filename_out = 'all_global_vul_pop.csv'
-    folder_out = os.path.join('data', 'processed', 'results' , 'csv')
-    path_out = os.path.join(folder_out, filename_out)
-    if not os.path.exists(path_out):
-        os.makedirs(path_out)
-    output.to_csv(path_out, index = False)
+            if country['income_group'] == 'HIC':
+                continue
+
+            if not country['iso3'] == 'BGD':
+                continue
+            filename_in = 'varifying_pop.csv'
+            folder_in = os.path.join('data', 'processed', iso3 , 'csv', scene)
+            path_in = os.path.join(folder_in, filename_in)
+            if not os.path.exists(path_in):
+                continue
+            
+            pop = pandas.read_csv(path_in)
+            pop = pop.to_dict('records')
+            output = output + pop
+
+        output = pandas.DataFrame(output)
+        filename_out = 'global_vul_pop.csv'
+        folder_out = os.path.join('data', 'processed', 'results' , 'csv', scene)
+        if not os.path.exists(folder_out):
+            os.makedirs(folder_out)
+        path_out = os.path.join(folder_out, filename_out)
+        output.to_csv(path_out, index = False)
